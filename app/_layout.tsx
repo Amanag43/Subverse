4242131c58bb57426855d63bf2638498f35ff8fa
Aman Agarwal <codeaman.18@gmail.com>
2026-04-09 20:05:30 +0530

    feat: project setup, native wind , routing, tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import "@/global.css";
 import { Stack } from "expo-router";
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="subscriptions" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
